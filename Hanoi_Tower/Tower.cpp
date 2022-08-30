1d77f8b98342416c7dd650654404596553b69bd6
strupeiq <alinka-roxy@mail.ru>
2022-08-30 21:07:54 +0500

    added select and draw

--- Hanoi_Tower/Tower.cpp
+++ Hanoi_Tower/Tower.cpp
@@ -9,92 +9,86 @@ const int WIDTH = 1200;
 const int HEIGHT = 900;
 
 int h = 80;
-int r = 30;
 int disk_width = 300;
+int max_r = 150;
+
+int x_0_1 = 200;
+int x_0_2 = 590;
+int x_0_3 = 980;
+int y_0 = HEIGHT;
 
 std::vector<int> first_pillar(5);
 std::vector<int> second_pillar(0);
 std::vector<int> third_pillar(0);
 
-int y_min_1 = HEIGHT - h * first_pillar.size();
-int y_min_2 = HEIGHT - h * second_pillar.size();
-int y_min_3 = HEIGHT - h * third_pillar.size();
+/*
+ѕервый диск из (x0-r0 - left; y0 - w - top) в точку (x0 + r0 - right; y0 - bottom)
+¬торой диск из (x0-r1; y0 Ц w * 2) в точку (x0+r1; y0 Ц w)
+x_0 - r[i] - left
+y_0 - h * (i + 1) - top
+x_0 + r[i] - right
+y_0 - h * i - bottom
+*/
+std::vector<int> r(5);
+
+void r_push() {
+    for (int i = 0; i < r.size(); ++i) {
+        r[i] = max_r;
+        max_r -= 30;
+    }
+}
 
 void draw_disks_1() {
-	/*Disk disk_1;
-	disk_1.left = 50;
-	disk_1.top = 820;
-	disk_1.right = 350;
-	disk_1.bottom = 900;
-	*/
+    for (int i = 0; i < first_pillar.size(); ++i) {
+        setfillstyle(SOLID_FILL, RGB(59, 153, 105));
+        bar(x_0_1 - r[i], y_0 - h * (i + 1), x_0_1 + r[i], y_0 - h * i);
+    }
+}
 
-	for (int i = 0; i < first_pillar.size(); ++i) {
-		first_pillar[i] = disk_width;
-		disk_width -= 60;
-	}
+void draw_disks_2() {
+    for (int i = 0; i < second_pillar.size(); ++i) {
+        setfillstyle(SOLID_FILL, RGB(59, 153, 105));
+        bar(x_0_2 - r[i], y_0 - h * (i + 1), x_0_2 + r[i], y_0 - h * i);
+    }
+}
 
-	for (int i = 0; i < first_pillar.size(); ++i) {
-		if (first_pillar[i] == 300) {
-			setfillstyle(SOLID_FILL, RGB(59, 153, 105));
-			bar(50, 820, 350, 900);
-		}
-		else if(first_pillar[i] == 240){
-			setfillstyle(SOLID_FILL, RGB(59, 153, 178));
-			bar(80, 740, 320, 820);
-			/*disk_1.left += r;
-			disk_1.top -= h;
-			disk_1.right -= r;
-			disk_1.bottom -= h;
-			*/
-		}
-		else if (first_pillar[i] == 180) {
-			setfillstyle(SOLID_FILL, RGB(59, 153, 105));
-			bar(110, 660, 290, 740);
-		}
-		else if (first_pillar[i] == 120) {
-			setfillstyle(SOLID_FILL, RGB(59, 153, 178));
-			bar(140, 580, 260, 660);
-		}
-		else if (first_pillar[i] == 60) {
-			setfillstyle(SOLID_FILL, RGB(59, 153, 105));
-			bar(170, 500, 230, 580);
-		}
-	}
+void draw_disks_3() {
+    for (int i = 0; i < third_pillar.size(); ++i) {
+        setfillstyle(SOLID_FILL, RGB(59, 153, 105));
+        bar(x_0_3 - r[i], y_0 - h * (i + 1), x_0_3 + r[i], y_0 - h * i);
+    }
 }
 
 int select_disk_1() {
-	int x = mousex();
-	int y = mousey();
+    int x = mousex();
+    int y = mousey();
 
-	if (x >= 200 - (first_pillar[first_pillar.size()] / 2) && x <= 200 + (first_pillar[first_pillar.size()] / 2)
-		&& y >= y_min_1 - h * first_pillar.size() && y <= y_min_1 - h * (first_pillar.size() - 1)) {
+    if (x >= x_0_1 - r[first_pillar.size() - 1] && x <= x_0_1 + r[first_pillar.size() - 1]
+        && y >= y_0 - h * first_pillar.size() - 1 && y <= y_0 - h * first_pillar.size()) {
 
-		return 1;
-	}
+        return 1;
+    }
 }
 
 int select_disk_2() {
-	int x = mousex();
-	int y = mousey();
+    int x = mousex();
+    int y = mousey();
 
-	if (x >= 600 - (second_pillar[second_pillar.size()] / 2) && x <= 600 + (second_pillar[second_pillar.size()] / 2)
-		&& y >= y_min_2 - h * second_pillar.size() && y <= y_min_2 - h * (second_pillar.size() - 1)) {
+    if (x >= x_0_2 - r[second_pillar.size() - 1] && x <= x_0_2 + r[second_pillar.size() - 1]
+        && y >= y_0 - r[second_pillar.size() - 1] && y <= y_0 + r[second_pillar.size() - 1]) {
 
-		return 2;
-	}
+        return 2;
+    }
 }
 
-int select_disk_3() {
-	int x = mousex();
-	int y = mousey();
+int selet_disk_3() {
+    int x = mousex();
+    int y = mousey();
 
-	if (x >= 1000 - (third_pillar[third_pillar.size()]) && x <= 1000 + (third_pillar[third_pillar.size()] / 2)
-		&& y >= y_min_3 - h * third_pillar.size() && y <= y_min_3 - h * (third_pillar.size() - 1)) {
+    if (x >= x_0_3 - r[third_pillar.size() - 1] && x <= x_0_3 + r[third_pillar.size() - 1]
+        && y >= y_0 - r[third_pillar.size() - 1] && y <= y_0 + r[third_pillar.size() - 1]) {
 
-		return 3;
-	}
+        return 3;
+    }
 }
 
-int check() {
-
-}
--- Hanoi_Tower/main.cpp
+++ Hanoi_Tower/main.cpp
@@ -7,33 +7,54 @@
 
 const int WIDTH = 1200;
 const int HEIGHT = 900;
-
+int disk_width = 300;
 
 int main() {
-	initwindow(WIDTH, HEIGHT, "Towers", 0, 0);
-	setfillstyle(SOLID_FILL, BROWN);
-	bar(180, 300, 220, 900);
-	bar(570, 300, 610, 900);                   //Отрисовка столбиков
-	bar(960, 300, 1000, 900);
+    initwindow(WIDTH, HEIGHT, "Towers", 0, 0, true);
+    r_push();
 
-	if (mousebuttons() == 1) {
-		int button = select_disk_1();
-		if (button == 1) {
-			std::cout << "yes";
-		}
-	}
-	std::vector<int> first_pillar(5);
-	std::vector<int> second_pillar(0);
-	std::vector<int> third_pillar(0);
+    std::vector<int> first_pillar(5);
+    std::vector<int> second_pillar(0);
+    std::vector<int> third_pillar(0);
 
-	/*for (int i = 0; i < first_pillar.size(); ++i) {
-		first_pillar[i] = disk_width;
-		disk_width -= 60;
-	}
-	*/
-	draw_disks_1();
+    for (int i = 0; i < first_pillar.size(); ++i) {
+        first_pillar[i] = disk_width;
+        disk_width -= 60;
 
-	getch();
+    }
+    for (int i = 0; i < second_pillar.size(); ++i) {
+        second_pillar[i] = disk_width;
+        disk_width -= 60;
+    }
+    for (int i = 0; i < third_pillar.size(); ++i) {
+        third_pillar[i] = disk_width;
+        disk_width -= 60;
+    }
 
-	return 0;
+    while (true) {
+        setbkcolor(BLACK);
+        clearviewport();
+
+        setfillstyle(SOLID_FILL, BROWN);
+        bar(180, 300, 220, 900);
+        bar(570, 300, 610, 900);                   //Отрисовка столбиков
+        bar(960, 300, 1000, 900);
+
+        draw_disks_1();
+        draw_disks_2();
+        draw_disks_3();
+
+        swapbuffers();
+        delay(10);
+
+
+        if (kbhit()) {
+            int key = getch();
+            if (key == KEY_ESC) {
+                break;
+            }
+            return 0;
+
+        }
+    }
 }
