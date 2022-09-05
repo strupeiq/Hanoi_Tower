d5b9ab51b0f572eff639f42123dff69734e08680
yarullina <110098827+strupeiq@users.noreply.github.com>
2022-09-05 22:54:08 +0500

    Update Tower.cpp

--- Hanoi_Tower/Tower.cpp
+++ Hanoi_Tower/Tower.cpp
@@ -9,14 +9,15 @@ const int WIDTH = 1200;
 const int HEIGHT = 900;
 
 int h = 80;
-int disk_width_1 = 300;
-int disk_width_2 = 300;
-int disk_width_3 = 300;
-int max_r = 150;
+
+int r_1 = 150;
+int r_2 = 150;
+int r_3 = 150;
 
 int x_0_1 = 200;
 int x_0_2 = 590;
 int x_0_3 = 980;
+
 int y_0 = HEIGHT;
 
 int f_n = 5;
@@ -29,7 +30,7 @@ std::vector<int> third_pillar(t_n);
 
 bool selectionFrom = true;
 bool selectionTo = false;
-bool selection = false;
+int from;
 
 /*
 Первый диск из (x0-r0 - left; y0 - w - top) в точку (x0 + r0 - right; y0 - bottom)
@@ -39,49 +40,42 @@ y_0 - h * (i + 1) - top
 x_0 + r[i] - right
 y_0 - h * i - bottom
 */
-std::vector<int> r(5);
 
 void vector_push() {
     for (int i = 0; i < first_pillar.size(); ++i) {
-        first_pillar[i] = disk_width_1;
-        disk_width_1 -= 60;
+        first_pillar[i] = r_1;
+        r_1 -= 30;
 
     }
     for (int i = 0; i < second_pillar.size(); ++i) {
-        second_pillar[i] = disk_width_2;
-        disk_width_2 -= 60;
+        second_pillar[i] = r_2;
+        r_2 -= 30;
     }
     for (int i = 0; i < third_pillar.size(); ++i) {
-        third_pillar[i] = disk_width_3;
-        disk_width_3 -= 60;
+        third_pillar[i] = r_3;
+        r_3 -= 30;
     }
 }
 
-void r_push() {
-    for (int i = 0; i < r.size(); ++i) {
-        r[i] = max_r;
-        max_r -= 30;
-    }
-}
 
 void draw_disks_1() {
     for (int i = 0; i < first_pillar.size(); ++i) {
         setfillstyle(SOLID_FILL, RGB(59, 153, 105));
-        bar(x_0_1 - r[i], y_0 - h * (i + 1), x_0_1 + r[i], y_0 - h * i);
+        bar(x_0_1 - first_pillar[i], y_0 - h * (i + 1), x_0_1 + first_pillar[i], y_0 - h * i);
     }
 }
 
 void draw_disks_2() {
     for (int i = 0; i < second_pillar.size(); ++i) {
         setfillstyle(SOLID_FILL, RGB(59, 153, 105));
-        bar(x_0_2 - r[i], y_0 - h * (i + 1), x_0_2 + r[i], y_0 - h * i);
+        bar(x_0_2 - second_pillar[i], y_0 - h * (i + 1), x_0_2 + second_pillar[i], y_0 - h * i);
     }
 }
 
 void draw_disks_3() {
     for (int i = 0; i < third_pillar.size(); ++i) {
         setfillstyle(SOLID_FILL, RGB(59, 153, 105));
-        bar(x_0_3 - r[i], y_0 - h * (i + 1), x_0_3 + r[i], y_0 - h * i);
+        bar(x_0_3 - third_pillar[i], y_0 - h * (i + 1), x_0_3 + third_pillar[i], y_0 - h * i);
     }
 }
 
@@ -89,10 +83,13 @@ int select_disk_1() {
     int x = mousex();
     int y = mousey();
 
-    if (x >= x_0_1 - r[first_pillar.size() - 1] && x <= x_0_1 + r[first_pillar.size() - 1]
+    if (x >= x_0_1 - first_pillar[first_pillar.size() - 1] && x <= x_0_1 + first_pillar[first_pillar.size() - 1]
         && y >= y_0 - h * (first_pillar.size() - 1) && y <= y_0 - h * first_pillar.size()) {
 
         return 1;
+    }
+    else {
+        return 0;
     }
 }
 
@@ -100,10 +97,13 @@ int select_disk_2() {
     int x = mousex();
     int y = mousey();
 
-    if (x >= x_0_2 - r[second_pillar.size() - 1] && x <= x_0_2 + r[second_pillar.size() - 1]
+    if (x >= x_0_2 - second_pillar[second_pillar.size() - 1] && x <= x_0_2 + second_pillar[second_pillar.size() - 1]
         && y >= y_0 - h * (second_pillar.size() - 1) && y <= y_0 - h * second_pillar.size()) {
 
         return 2;
+    }
+    else {
+        return 0;
     }
 }
 
@@ -111,34 +111,51 @@ int select_disk_3() {
     int x = mousex();
     int y = mousey();
 
-    if (x >= x_0_3 - r[third_pillar.size() - 1] && x <= x_0_3 + r[third_pillar.size() - 1]
+    if (x >= x_0_3 - third_pillar[third_pillar.size() - 1] && x <= x_0_3 + third_pillar[third_pillar.size() - 1]
         && y >= y_0 - h * (third_pillar.size() - 1) && y <= y_0 - h * third_pillar.size()) {
 
         return 3;
     }
+    else {
+        return 0;
+    }
 }
 
 void game() {
+
+    setfillstyle(SOLID_FILL, BROWN);
+    bar(180, 300, 220, 900);
+    bar(570, 300, 610, 900);                   //Отрисовка колышков
+    bar(960, 300, 1000, 900);
+
+    draw_disks_1();
+    draw_disks_2();
+    draw_disks_3();
+
+    //Проверка 
     if (selectionFrom) {
         if (select_disk_1()) {
             setfillstyle(SOLID_FILL, RED);
-            bar(x_0_1 - r[first_pillar.size() - 1], y_0 - h * first_pillar.size(), x_0_1 + r[first_pillar.size() - 1], y_0 - h * (first_pillar.size() - 1));
+            bar(x_0_1 - first_pillar[first_pillar.size() - 1], y_0 - h * first_pillar.size(), x_0_1 + first_pillar[first_pillar.size() - 1], y_0 - h * (first_pillar.size() - 1));
+            from = 1;
         }
         else if (select_disk_2()) {
             setfillstyle(SOLID_FILL, RED);
-            bar(x_0_2 - r[second_pillar.size() - 1], y_0 - h * second_pillar.size(), x_0_2 - r[second_pillar.size() - 1], y_0 - h * (second_pillar.size() - 1));
-            selection = select_disk_2();
+            bar(x_0_2 - second_pillar[second_pillar.size() - 1], y_0 - h * second_pillar.size(), x_0_2 - second_pillar[second_pillar.size() - 1], y_0 - h * (second_pillar.size() - 1));
+            from = 2;
         }
         else if (select_disk_3()) {
             setfillstyle(SOLID_FILL, RED);
-            bar(x_0_3 - r[third_pillar.size() - 1], y_0 - h * third_pillar.size(), x_0_3 - r[third_pillar.size() - 1], y_0 - h * (third_pillar.size() - 1));
-            selection = select_disk_3();
+            bar(x_0_3 - third_pillar[third_pillar.size() - 1], y_0 - h * third_pillar.size(), x_0_3 - third_pillar[third_pillar.size() - 1], y_0 - h * (third_pillar.size() - 1));
+            from = 3;
         }
         selectionFrom = false;
         selectionTo = true;
     }
+
     else if (selectionTo) {
-        if (select_disk_1()) {
+
+        if (from == 1) {
             if (select_disk_2()) {
                 if (first_pillar[first_pillar.size() - 1] < second_pillar[second_pillar.size() - 1]) {
                     second_pillar.resize(s_n++);
@@ -154,24 +171,25 @@ void game() {
                 }
             }
         }
-    }
-    else if (select_disk_2()) {
-        if (select_disk_1()) {
-            if (second_pillar[second_pillar.size() - 1] < first_pillar[first_pillar.size() - 1]) {
+
+        else if (from == 2) {
+            if (select_disk_1()) {
+                if (second_pillar[second_pillar.size() - 1] < first_pillar[first_pillar.size() - 1]) {
                     first_pillar.resize(f_n++);
                     first_pillar.push_back(second_pillar[second_pillar.size() - 1]);
                     second_pillar.pop_back();
                 }
             }
-        else if (select_disk_3()) {
-            if (second_pillar[second_pillar.size() - 1] < third_pillar[third_pillar.size() - 1]) {
+            else if (select_disk_3()) {
+                if (second_pillar[second_pillar.size() - 1] < third_pillar[third_pillar.size() - 1]) {
                     third_pillar.resize(t_n++);
                     third_pillar.push_back(second_pillar[second_pillar.size() - 1]);
                     second_pillar.pop_back();
-              }
+                }
             }
         }
-        else if (select_disk_3()) {
+
+        else if (from == 3) {
             if (select_disk_1()) {
                 if (third_pillar[third_pillar.size() - 1] < first_pillar[first_pillar.size() - 1]) {
                     first_pillar.resize(f_n++);
@@ -187,6 +205,8 @@ void game() {
                 }
             }
         }
-    selectionTo = false;
-    selectionFrom = true;
+
+        selectionTo = false;
+        selectionFrom = true;
     }
+}
